d99da47597cb81f2f2b8de00d1db92985cbc4a8c
TeamLewok <42891947+TeamLewok@users.noreply.github.com>
2018-12-22 10:45:03 +0530

    33 Adding menu items in the app bar PRK

--- app/src/main/java/dragon/bakuman/iu/appbarexampleprk/MainActivity.java
+++ app/src/main/java/dragon/bakuman/iu/appbarexampleprk/MainActivity.java
@@ -3,6 +3,8 @@ package dragon.bakuman.iu.appbarexampleprk;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +22,15 @@ public class MainActivity extends AppCompatActivity {
         mToolbar = findViewById(R.id.toolBar);
         setSupportActionBar(mToolbar);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.app_bar_menu, menu);
+
+
+        return true;
     }
 }
